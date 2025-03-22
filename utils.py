f00f8f60aa8033232a9797d6c21592fc83fde7ed
parvvaresh <parvvaresh@gmail.com>
2025-03-22 15:56:21 +0330

    load pre-trained model

---
+++ utils.py
@@ -0,0 +1,29 @@
+import pickle
+import pandas as pd
+
+def load_model_and_predict(input_data):
+    """
+    Load saved model and make prediction on new data
+    """
+    try:
+        # Load the trained model
+        with open('model/best_linear_model.pkl', 'rb') as file:
+            model = pickle.load(file)
+
+
+        # Load the scaler model
+        # with open('model/scaler.pkl', 'rb') as f:
+        #     scaler = pickle.load(f)
+
+        # Use the loaded scaler
+        # input_data = scaler.transform(input_data)
+
+            
+
+        # Make prediction
+        prediction = model.predict(input_data)
+        return prediction[0]
+    
+    except Exception as e:
+        print(f"Prediction error: {str(e)}")
+        return None
